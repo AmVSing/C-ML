4a3956209c3b6f0793c570af3712db682d49f212
Singaravelou, Amrithvelu <amrith.singaravelou25@imperial.ac.uk>
2026-06-08 17:36:47 +0100

    chore: added documentation and made return types consistent

--- src/tensors/tensor.h
+++ src/tensors/tensor.h
@@ -20,18 +20,19 @@ typedef struct tensor_s {
 // create
 Tensor make_tensor(size_t rank, const size_t shape[]); // create tensor, mallocd
 Tensor tensor_from_data(size_t rank, const size_t shape[], float* data); // create tensor from data
-void free_tensor(const Tensor* t); 
+void free_tensor(Tensor* t); 
 
 // other helpful funcs
-void tensor_fill(Tensor* t, float value);
-void tensor_rand(Tensor* t, float min, float max); // creates tensor populated with randfloats from [min, max]
+Tensor* tensor_fill(Tensor* t, float value); // create tensor with all elements set to value
+Tensor* tensor_zeros(size_t rank, const size_t shape[]); // returns a 0 tensor
+Tensor* tensor_rand(Tensor* t, float min, float max); // creates tensor populated with randfloats from [min, max]
 
 /* ops */
 
 // shape comparison funcs
-bool tensor_addable(Tensor* t1, Tensor* t2); // true iff t1 and t2 have the same shape
-bool tensor_multiplicable(Tensor* t1, Tensor* t2); // true iff t1 and t2 are dot product compatible
-bool is_matrix(Tensor* t); // true iff t points to a matrix
+bool tensor_addable(const Tensor* t1, const Tensor* t2); // true iff t1 and t2 have the same shape
+bool tensor_multiplicable(const Tensor* t1, const Tensor* t2); // true iff t1 and t2 are dot product compatible
+bool is_matrix(const Tensor* t); // true iff t points to a matrix
 
 // actual ops - (tensor, scalar)
 Tensor* tensor_add_scalar(const Tensor* t, float x, Tensor* out); // adds x to all entries
@@ -40,11 +41,13 @@ Tensor* tensor_mult_scalar(const Tensor* t, float x, Tensor* out); // multiples 
 // actual ops - (tensor, tensor)
 Tensor* tensor_add(const Tensor* t1, const Tensor* t2, Tensor* res);
 Tensor* tensor_sub(const Tensor* t1, const Tensor* t2, Tensor* res);
-Tensor* tensor_mult(const Tensor* t1, const Tensor* t2, Tensor* res);
+Tensor* tensor_mult(const Tensor* t1, const Tensor* t2, Tensor* res); // elementwise multiplication not dot product
 Tensor* tensor_div(const Tensor* t1, const Tensor* t2, Tensor* res);
 
 // matrix ops
 Tensor* matmul(const Tensor* a, const Tensor* b, Tensor* out); 
 Tensor* transpose(const Tensor* a, Tensor* out);
+
+/* display helpers */
 
 #endif
